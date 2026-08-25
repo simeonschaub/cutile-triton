26b0d19410120991f6cfb5cc84a574e12dffa27c
Simeon David Schaub <simeon.schaub@enpc.fr>
2026-08-25 18:54:41 +0200

    wip

--- bench/spmm_rptr_human.jl
+++ bench/spmm_rptr_human.jl
@@ -2,18 +2,22 @@ import cuTile as ct
 using CUDA
 using CUDA: i32
 
-struct SplitRangeCSRMatrix{T, Ti, V <: AbstractVector{T}, Vi <: AbstractVector{Ti}} <: AbstractMatrix{T}
-    contiguous_ptr::Vi
+struct SplitRangeCSRMatrix{T, Ti, V <: AbstractVector{T}, Vi1 <: AbstractVector{Ti}, Vi2 <: AbstractVector{Ti}} <: AbstractMatrix{T}
+    contiguous_ptr::Vi1
     contiguous_vals::V
-    scattered_ptr::Vi
-    scattered_inds::Vi
+    scattered_ptr::Vi1
+    scattered_inds::Vi2
     scattered_vals::V
 end
+
+Adapt.@adapt_structure SplitRangeCSRMatrix
+Base.size(A::SplitRangeCSRMatrix) = (length(A.contiguous_ptr) - 1, length(A.contiguous_vals))
 
 function spmm_rc_kernel(C, A::SplitRangeCSRMatrix{T, I}, B, α, β, TILE_M, TILE_N, TILE_K, BETA_NZ) where {T, I}
     (; contiguous_ptr, contiguous_vals, scattered_ptr, scattered_inds, scattered_vals) = A
     m, n = ct.bid(1), ct.bid(2)
     row_indices = (m - I(1)) * TILE_M .+ ct.arange(TILE_M)
+    col_indices = reshape((n - I(1)) * TILE_N .+ ct.arange(TILE_N), 1, 1, TILE_N)
 
     k₀ = reshape(ct.arange(TILE_K), 1, TILE_K)
 
@@ -32,19 +36,25 @@ function spmm_rc_kernel(C, A::SplitRangeCSRMatrix{T, I}, B, α, β, TILE_M, TILE
         k = (t - I(1)) * TILE_K .+ k₀
 
         contiguous_mask = k .≤ contiguous_length
-        contiguous_acc = ct.gather(contiguous_vals, contiguous_offset .+ k; mask = contiguous_mask)
+        contiguous_i = contiguous_offset .+ k
+        contiguous_vals = ct.gather(contiguous_vals, contiguous_i; mask = contiguous_mask, padding_value = zero(T))
 
         scattered_ptrs = scattered_offset .+ (k .- contiguous_length)
         scattered_mask = contiguous_length .< k .≤ contiguous_length .+ scattered_length
-        scattered_acc = ct.gather(scattered_vals, scattered_ptrs; mask = scattered_mask)
+        scattered_i = ct.gather(scattered_inds, scattered_ptrs; mask = scattered_mask, padding_value = I(0))
+        scattered_vals = ct.gather(scattered_vals, scattered_ptrs; mask = scattered_mask, padding_value = zero(T))
 
-        acc = acc .+ contiguous_acc .+ scattered_acc
+        i = ifelse.(contiguous_mask, contiguous_i, scattered_i)
+        b_vals = ct.gather(B, (reshape(i, TILE_M, TILE_K), col_indices))
+        dot = dropdims(sum((contiguous_vals .+ scattered_vals) .* b_vals; dims = 2); dims = 2)
+
+        acc = acc .+ dot
     end
 
     res = α .* acc
     if BETA_NZ
         res = res .+ β .* ct.load(C, (m, n), (TILE_M, TILE_N))
     end
-    ct.store!(C, (m, n), res)
+    ct.store(C, (m, n), res)
 end
 
