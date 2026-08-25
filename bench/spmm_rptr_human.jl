1e738bde2d240e7fdfcc83eb19a763038fc313ae
Simeon David Schaub <simeon.schaub@enpc.fr>
2026-08-25 19:34:47 +0200

    wip

--- bench/spmm_rptr_human.jl
+++ bench/spmm_rptr_human.jl
@@ -2,13 +2,15 @@ import cuTile as ct
 using CUDA
 using CUDA: i32
 
-struct SplitRangeCSRMatrix{T, Ti, V <: AbstractVector{T}, Vi1 <: AbstractVector{Ti}, Vi2 <: AbstractVector{Ti}} <: AbstractMatrix{T}
+struct SplitRangeCSRMatrix{T, Ti, V, Vi1, Vi2} <: AbstractMatrix{T}
     contiguous_ptr::Vi1
     contiguous_vals::V
     scattered_ptr::Vi1
     scattered_inds::Vi2
     scattered_vals::V
 end
+SplitRangeCSRMatrix(cp::Vi1, cv::V, sp::Vi1, si::Vi2, sv::V) where {V, Vi1, Vi2} =
+    SplitRangeCSRMatrix{eltype(V), eltype(Vi1), V, Vi1, Vi2}(cp, cv, sp, si, sv)
 
 Adapt.@adapt_structure SplitRangeCSRMatrix
 Base.size(A::SplitRangeCSRMatrix) = (length(A.contiguous_ptr) - 1, length(A.contiguous_vals))
@@ -29,24 +31,24 @@ function spmm_rc_kernel(C, A::SplitRangeCSRMatrix{T, I}, B, α, β, TILE_M, TILE
     scattered_offset = scattered_start .- I(1)
     scattered_length = ct.gather(scattered_ptr, row_indices .+ I(1)) .- scattered_start
 
-    total_iters = cld(maximum(contiguous_length .+ scattered_length), TILE_K)
+    total_iters = cld(maximum(contiguous_length .+ scattered_length), I(TILE_K))
     acc = zeros(T, TILE_M, TILE_N)
 
     for t in I(1):total_iters
-        k = (t - I(1)) * TILE_K .+ k₀
+        k = (t - I(1)) * I(TILE_K) .+ k₀
 
         contiguous_mask = k .≤ contiguous_length
         contiguous_i = contiguous_offset .+ k
-        contiguous_vals = ct.gather(contiguous_vals, contiguous_i; mask = contiguous_mask, padding_value = zero(T))
+        contiguous_v = ct.gather(contiguous_vals, contiguous_i; mask = contiguous_mask, padding_value = zero(T))
 
         scattered_ptrs = scattered_offset .+ (k .- contiguous_length)
         scattered_mask = contiguous_length .< k .≤ contiguous_length .+ scattered_length
         scattered_i = ct.gather(scattered_inds, scattered_ptrs; mask = scattered_mask, padding_value = I(0))
-        scattered_vals = ct.gather(scattered_vals, scattered_ptrs; mask = scattered_mask, padding_value = zero(T))
+        scattered_v = ct.gather(scattered_vals, scattered_ptrs; mask = scattered_mask, padding_value = zero(T))
 
         i = ifelse.(contiguous_mask, contiguous_i, scattered_i)
         b_vals = ct.gather(B, (reshape(i, TILE_M, TILE_K), col_indices))
-        dot = dropdims(sum((contiguous_vals .+ scattered_vals) .* b_vals; dims = 2); dims = 2)
+        dot = dropdims(sum((contiguous_v .+ scattered_v) .* b_vals; dims = 2); dims = 2)
 
         acc = acc .+ dot
     end
